a0367a424e9ed779815c731c9d711feee3717bfe
Ubuntu <ubuntu@ip-172-31-28-83.us-east-2.compute.internal>
2023-10-20 00:34:06 +0000

    implementing SSL

--- tools/generateCert.sh
+++ tools/generateCert.sh
@@ -2,7 +2,8 @@
 
 # Configuration
 KEY_SIZE=2048
-CERT_COMMON_NAME=localhost, http://ec2-18-222-136-174.us-east-2.compute.amazonaws.com
+CERT_COMMON_NAME=localhost
+SAN=("DNS:localhost" "DNS:ec2-18-222-136-174.us-east-2.compute.amazonaws.com")
 KEYSTORE_ALIAS=Sudoku
 KEYSTORE_PASSWORD=Sudoku
 
@@ -22,7 +23,7 @@ extendedKeyUsage = serverAuth
 subjectAltName = @alt_names
 
 [alt_names]
-DNS.1 = $CERT_COMMON_NAME
+$(for ((i = 1; i <= ${#SAN[@]}; i++)); do echo "DNS.$i = ${SAN[$i-1]}"; done)
 EOF
 
 # Generate a new key pair and CSR
@@ -40,12 +41,3 @@ openssl pkcs12 -export -in localhost-certificate.pem -inkey localhost-private-ke
 # Import the PKCS12 file into a Java keystore (using the same password)
 keytool -importkeystore -srckeystore localhost.p12 -srcstoretype PKCS12 -destkeystore localhost.jks -deststoretype JKS -deststorepass $KEYSTORE_PASSWORD
 
-mv localhost.p12 ssl
-mv localhost-private-key.pem ssl
-mv localhost-certificate.pem ssl
-mv localhost-certificate.crt ssl
-mv localhost.csr ssl
-mv localhost.jks ssl
-mv openssl.cnf ssl
-
-echo "Certificate generation and import completed."
